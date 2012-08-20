de726b35b907b1f1c5a8562b959fb9b2c0ba9a07
Andrew Havens <andrew.havens@evanta.com>
2012-08-20 14:37:51 -0700

    renamed to challenges and added hints when losing

---
+++ challenges.js
@@ -0,0 +1,16 @@
+var challenges = [
+	{
+		nodes: '<span class="good">Good</span> <span class="bad">Bad</span>',
+		text: 'Good Bad',
+		time_limit: 7000,
+		answer: ['Good'],
+		hint: 'Regular expressions are case sensitive'
+	},
+	{
+		nodes: '<span class="good">good</span> <span class="bad">Good</span>',
+		text: 'good Good',
+		time_limit: 7000,
+		answer: ['good'],
+		hint: 'Regular expressions are case sensitive'
+	}
+];
--- game.js
+++ game.js
@@ -8,43 +8,29 @@ $(function(){
 	});
 });
 
-var current_task = -1;
+var current_challenge = -1;
 var solved = false;
-var tasks = [
-	{
-		nodes: '<span class="good">Good</span> <span class="bad">Bad</span>',
-		text: 'Good Bad',
-		time_limit: 7000,
-		answer: ['Good']
-	},
-	{
-		nodes: '<span class="good">good</span> <span class="bad">Good</span>',
-		text: 'good Good',
-		time_limit: 7000,
-		answer: ['good']
-	}
-];
 var running = true;
 
 function begin(){
-	current_task = -1;
+	current_challenge = -1;
 	$('.message').fadeOut();
 	$('#input').fadeIn(function(){
-		display_next_task();
+		display_next_challenge();
 	});
 }
 
-function display_next_task(){
-	setup_next_task();
-	var task = tasks[current_task]
-	var $html = create_task_html(task);
-	$html.appendTo('#game').animate({top: '100%'}, task.time_limit, 'linear', function(){
+function display_next_challenge(){
+	setup_next_challenge();
+	var challenge = challenges[current_challenge]
+	var $html = create_challenge_html(challenge);
+	$html.appendTo('#game').animate({top: '100%'}, challenge.time_limit, 'linear', function(){
 		$html.remove();
 		if (solved) {
-			if (current_task == tasks.length - 1) {
+			if (current_challenge == challenges.length - 1) {
 				game_won();
 			} else {
-				display_next_task();
+				display_next_challenge();
 			}
 		} else {
 			game_lost();
@@ -52,22 +38,22 @@ function display_next_task(){
   	});
 }
 
-function setup_next_task(){
-	current_task++;
+function setup_next_challenge(){
+	current_challenge++;
 	solved = false;
 	$('#status').children().hide();
 	$('#input').attr('disabled', false);
 }
 
-function create_task_html(task){
-	return $('<div class="task">' + task.nodes + '</div>');
+function create_challenge_html(challenge){
+	return $('<div class="challenge">' + challenge.nodes + '</div>');
 }
 
 function guess(){
-	var task = tasks[current_task]
+	var challenge = challenges[current_challenge]
 	var regex = new RegExp($('#input').val());
-	var guess = regex.exec(task.text);
-	var answer = task.answer
+	var guess = regex.exec(challenge.text);
+	var answer = challenge.answer
 	if (guess && guess.length == answer.length) {
 		var correct = true;
 		for (var i = 0; i < guess.length; i++) {
@@ -81,7 +67,7 @@ function guess(){
 			$('#status .bad').hide();
 			$('#status .good').show();
 			$('#input').val('').attr('disabled', true);
-			$('.task .good').animate({
+			$('.challenge .good').animate({
 				opacity: 0,
 				top: '-100px'
 			}, 500);
@@ -96,6 +82,7 @@ function game_won(){
 }
 
 function game_lost(){
+	$('#game_lost .hint').html(challenges[current_challenge].hint);
 	show_message($('#game_lost'));
 }
 
